374a3e02fcadd0e6142e3a2e3493f35ea2bf3265
Yusufcan <76500767+YusufCSert@users.noreply.github.com>
2022-01-01 13:28:16 +0200

    Adding options to argumnes if -d then next argument dir

--- server.c
+++ server.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv)
+
+int main(int argc, char **argv)
 {
+    char *dir;
+
+
+
+
 	if(argc == 7){
-		printf("%d",argc);
+		
+		for(int i=0; i < argc; i++)
+		{
+			if(strcmp("-d",argv[i]) == 0){
+                dir = argv[i+1];
+                printf("%s\n",dir);
+            }
+		}
 	}
 	else
 	{
